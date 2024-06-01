bc4d2b6e787faa86358d1cafa60bef20dc5ae8a3
redgp0g <guilhermeg2004@gmail.com>
2024-06-01 14:11:49 -0300

    Alterado Texto

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -35,7 +35,7 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Mágica Dinâmica de Site em Next.js
+            Site Dinâmico em Next.js
           </h2>
             <TextGenerateEffect
               className="text-center text-[40px] md:text-5xl lg:text-6xl mb-5"
