26ecb62a0676d6a1503a0c5cbb1a24d245e6992a
redgp0g <guilhermeg2004@gmail.com>
2024-07-13 14:34:56 -0300

    :construction: Iniciar Montagem tech stack

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -98,6 +98,23 @@ export const BentoGridItem = ({
         </div>
 
         {id === 2 && <GlobeDemo />}
+        {id === 3 && (
+          <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+            <div className="flex flex-col gap-3 lg:gap-8">
+              {["React.js", "Next.js", "Typescript", "TailwindCSS"].map(
+                (item) => (
+                  <span
+                    key={item}
+                    className="py-2 lg:py-4 lg:px-3 px-3 text-sx lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                )
+              )}
+              <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]"></span>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
